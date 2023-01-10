19e852cf31d68c9f75fb6d609acf703491633f4e
andri <andri@LAPTOP-NBRIT72P>
2023-01-10 18:37:23 +0100

    Aggiungere i file di progetto.

---
+++ Generazione tabellone definitivo/Program.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generazione_delle_cartelle_definitivo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //INIZIO CODICE
+            Console.Clear();
+            Console.SetCursorPosition((Console.WindowWidth - "IL GIOCO DELLA TOMBOLA PER 2 GIOCATORI".Length) / 3, 0);
+            Console.WriteLine("IL GIOCO DELLA TOMBOLA PER 2 GIOCATORI");
+            Console.WriteLine("- ");
+
+            //funzione random
+            Random random = new Random();
+
+            //generazione delle due cartelle
+
+            //1 dichiarazione delle due cartelle
+            int[,] cartella1 = new int[3, 9];
+
+            //lista che permette di non far rigenerare un numero due volte
+            List<int> numeri = new List<int>();
+
+            //generazione 1 cartella
+            Console.WriteLine(" CARTELLA PRIMO GIOCATORE ");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    int num;
+                    do
+                    {
+                        num = random.Next(1, 91);
+                    } while (numeri.Contains(num));
+                    numeri.Add(num);
+                    cartella1[i, j] = num;
+                }
+            }
+
+            //ciclo for per test dei valori1 e per posizionarli in righe e colonne
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(cartella1[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+            //generazione 2 cartella
+            Console.WriteLine("- ");
+            Console.WriteLine(" CARTELLA SECONDO GIOCATORE ");
+            int[,] cartella2 = new int[3, 9];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    int num;
+                    do
+                    {
+                        num = random.Next(1, 91);
+                    } while (numeri.Contains(num));
+                    numeri.Add(num);
+                    cartella2[i, j] = num;
+                }
+            }
+
+            //ciclo for per test dei valori1 e per posizionarli in righe e colonne
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    Console.Write(cartella2[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+
+
+
+
+
+
+            //generazione del tabellone del tabellone 
+
+            Console.WriteLine("- ");
+            Console.WriteLine("TABELLONE ");
+            //dichiarazione tabellone
+            int[,] tabellone = new int[9, 10];
+
+            //stampa tabellone
+            StampaTabellone(tabellone); //semplificazione del codice con funzione
+        }
+
+
+
+
+
+
+
+
+        //funzione per ottimizzare il codice
+        private static void StampaTabellone(int[,] tabellone)
+        {
+            //generazione tabellone aggiornato
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Console.Write(tabellone[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Generazione tabellone definitivo/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Le informazioni generali relative a un assembly sono controllate dal seguente 
+// set di attributi. Modificare i valori di questi attributi per modificare le informazioni
+// associate a un assembly.
+[assembly: AssemblyTitle("Generazione tabellone definitivo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Generazione tabellone definitivo")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Se si imposta ComVisible su false, i tipi in questo assembly non saranno visibili
+// ai componenti COM. Se è necessario accedere a un tipo in questo assembly da
+// COM, impostare su true l'attributo ComVisible per tale tipo.
+[assembly: ComVisible(false)]
+
+// Se il progetto viene esposto a COM, il GUID seguente verrà utilizzato come ID della libreria dei tipi
+[assembly: Guid("dd993ec6-dda3-4db7-bf7c-08e6a73239a6")]
+
+// Le informazioni sulla versione di un assembly sono costituite dai seguenti quattro valori:
+//
+//      Versione principale
+//      Versione secondaria
+//      Numero di build
+//      Revisione
+//
+// È possibile specificare tutti i valori oppure impostare valori predefiniti per i numeri relativi alla revisione e alla build
+// usando l'asterisco '*' come illustrato di seguito:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
